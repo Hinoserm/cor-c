b6d107e3c8e9a7d56e263b2f68ec368d9746ad0b
Hinoserm <mpratte@makefox.group>
2026-09-18 09:32:14 -0500

    Exercise exported ABI protection through the public optimizer pass

--- compiler/tests/metadata/ExportBoundaryTests.cs
+++ compiler/tests/metadata/ExportBoundaryTests.cs
@@ -9,15 +9,17 @@ public static class ExportBoundaryTests
     {
         Module module = new("open-unit") { Entry = "entry", PreserveExports = true };
         Function helper = new("helper", IrType.I32);
+        helper.Params.Add(helper.NewReg(IrType.I32));
         helper.NewBlock().Instrs.Add(new Instr { Op = Opcode.Ret, Operands = { new ImmOperand(42, IrType.I32) } });
         Function entry = new("entry", IrType.Void);
         var block = entry.NewBlock();
         block.Instrs.Add(new Instr { Op = Opcode.Store, Size = 4, Operands = { new SymOperand("state"), new ImmOperand(42, IrType.I32) } });
-        block.Instrs.Add(new Instr { Op = Opcode.Call, Callee = "helper", Dest = entry.NewReg(IrType.I32) });
+        block.Instrs.Add(new Instr { Op = Opcode.Call, Callee = "helper", Dest = entry.NewReg(IrType.I32), Operands = { new ImmOperand(1, IrType.I32) } });
         block.Instrs.Add(new Instr { Op = Opcode.Ret });
         module.Functions.Add(entry); module.Functions.Add(helper);
         module.Data.Add(new DataItem("state", new byte[4]) { Zero = true });
-        if (new ClosedFunctionCalls(module).CanChange(helper, out _)) throw new Exception("Exported ABI treated as closed world");
+        new DeadArguments().Run(module);
+        if (helper.Params.Count != 1) throw new Exception("Exported ABI treated as closed world");
         new DeadStatics().Run(module);
         if (module.Data.Count != 1 || !block.Instrs.Any(i => i.Op == Opcode.Store)) throw new Exception("External reader's state removed");
         new Inline().Run(module);
